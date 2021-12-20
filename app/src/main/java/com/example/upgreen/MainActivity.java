63557b92bfb0de48fa5d99e86e0524f640abcb4b
itsceallach <itsceallach@gmail.com>
2021-12-20 23:33:26 +0800

    UpGreen App

--- app/src/main/java/com/example/upgreen/MainActivity.java
+++ app/src/main/java/com/example/upgreen/MainActivity.java
@@ -2,10 +2,20 @@ package com.example.upgreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH = 3300;
+
+    Animation topAnim;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +23,19 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_anim);
+        imageView = findViewById(R.id.logo);
+
+        imageView.setAnimation(topAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH);
     }
 }
